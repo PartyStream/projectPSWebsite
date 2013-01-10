2dca9fee0f761931f3146a304d43badb34f66f2a
Salvatore D'Agositno <toto19@gmail.com>
2013-01-09 21:16:44 -0500

    now storing IP of client
    cleaned up comments

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,13 @@ exports.index = function(req, res){
 +
 +   \author Salvatore D'Agostino
 +   \date  2013-01-01 23:45
-+   \param email   The email address
++   \param email      The email address
++   \param headers    The HTTP headers from the request
++   \param ip         The remote IP of the client
 +   \param response   The result to be returned
++   \param client     The DB client connection
 **/
-function register(email,headers,response,client)
+function register(email,headers,ip,response,client)
 {
   console.log('Register user:');
   console.dir(email);
@@ -28,7 +31,7 @@ function register(email,headers,response,client)
   query = client.query({
     name: 'register user',
     text: "INSERT INTO registeredUsers(email, host,user_agent,date) values($1, $2,$3,current_timestamp)",
-    values: [email, headers.host, headers['user-agent']]
+    values: [email, ip, headers['user-agent']]
   });
 
   query.on('error',function(err) {

9a8c21d4c3f40aaa60a52afe8043375e673dd1cf
Salvatore D'Agositno <toto19@gmail.com>
2013-03-24 22:15:13 -0400

    added sitemap function

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,44 @@ exports.googleVerification = function (req,res){
 
 };// END function googleVerification
 
+/**
++   \brief siteMap
++
++       This function will echo the sitemap
++
++   \author Salvatore D'Agostino
++   \date  2013-03-24 22:03
++   \param req   The request
++   \param res   The result
++
++   \return NA
+**/
+function siteMap(req,res)
+{
+  console.log('SiteMap');
+  // this is the source of the URLs on your site, in this case we use a simple array, actually it could come from the database
+  var urls = ['index.html'];
+  // the root of your website - the protocol and the domain name with a trailing slash
+  var root_path = 'http://www.partystreamapp.com/';
+  // XML sitemap generation starts here
+  var priority = 0.5;
+  var freq = 'monthly';
+  var xml = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+  for (var i in urls) {
+      xml += '<url>';
+      xml += '<loc>'+ root_path + urls[i] + '</loc>';
+      xml += '<changefreq>'+ freq +'</changefreq>';
+      xml += '<priority>'+ priority +'</priority>';
+      xml += '</url>';
+      i++;
+  }
+  xml += '</urlset>';
+
+  res.header('Content-Type', 'text/xml');
+  res.send(xml);
+}// END function siteMap
+exports.siteMap = siteMap;
+
 
 /**
 +   \brief register

cbec075cda7b99aa66f9d03fafefa1870940a693
Salvatore D'Agositno <toto19@gmail.com>
2014-01-02 14:29:51 -0500

    exported app for mocha

--- web.js
+++ web.js
@@ -71,3 +71,4 @@ var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
+module.exports = app;
